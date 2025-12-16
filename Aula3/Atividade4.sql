982e51c11b6c940001bbe18c33cbd002bb0f5837
Igor De Azevedo Xavier Damieri De Morães <pet0160427@aluno.firjansenaisesi.com.br>
2025-12-15 21:32:04 -0300

    atividade4

--- Aula3/Atividade4.sql
+++ Aula3/Atividade4.sql
@@ -17,20 +17,75 @@ f) Listar o nome do usuário  e as datas em que ele comprou algo e filtrar por u
 g) Listar o nome dos usuários que não tiveram nenhuma compra;
 h) Listar o identificador e as datas das vendas que não tiveram um usuário;
 
+
+drop table produtos;
+drop table fornecedores ;
+drop table vendas;
+drop table usuarios;
+
+
 create table produtos(
 	id_produto serial primary key,
 	nome varchar(100) not null,
-	valor numeric (7,2),
+	valor numeric (7,2) not null,
 	estoque int default (5),
-	
+	fk_fornecedor int not null unique references fornecedores (id_fornecedor) --Chave estrangeira sempre tem a relação 1 para N ou N para 1, nesse caso 1 produto tem 1 fornecedor
+																			  --e uma fornecedor pode ter vários produtos
 );
 
 create table fornecedores(
 	id_fornecedor serial primary key,
 	nome varchar(100) not null,
-	cnpj char(14) unique,
 	local varchar (50),
-	
+	material varchar(50)
 );
 
-create table vendas
+create table vendas(
+	id_venda serial primary key,
+	codigo_produto int not null unique references produtos(id_produto),
+	data_venda timestamp default now(),
+	tipo_pagamento varchar(20),
+	fk_usuario int not null references usuarios(id_usuario)
+);
+
+create table usuarios(
+	id_usuario serial primary key,
+	nome varchar(100) not null,
+	cpf char (11) unique not null,
+	idade int check(idade >18)
+);
+
+insert into fornecedores (nome, local, material)
+values('fornecedor A', 'Rio de Janeiro', 'madeira'),
+('fornecedor B', 'Manaus', 'borracha'),
+('fornecedor C', 'São Paulo', 'calçados'),
+('fornecedor D', 'Porto Alegre' , 'veículos'),
+('fornecedor E', 'Belo Horizonte' , 'carne');
+
+
+insert into produtos (nome, valor, fk_fornecedor)
+values('Tábua de madeira', 12.50, 1),
+('pneu' , 150.90 , 2),
+('tênis', 185.90, 3),
+('fusca', 49999.00, 4),
+('picanha', 79.90, 5);
+
+insert into vendas (codigo_produto , tipo_pagamento, fk_usuario)
+values(5, 'pix', 3),
+(1, 'cartão de crédito', 5),
+(4, 'parcelamento', 2),
+(3, 'dinheiro', 5),
+(2, 'pix', 1);
+
+insert into usuarios (nome, cpf, idade)
+values('Marinalva', '55588899944', 20),
+('Ana', '99988877744', 30),
+('Jorge', '22233311155' , 19),
+('Berenice', '99911133322', 21),
+('Manoel', '22233366611', 45);
+
+
+select * from produtos;
+select * from fornecedores where nome = 'fornecedor E';
+select * from produtos where valor between 60.00 and 200.00;
+select produtos.nome, fornecedores.nome from produtos join fornecedores on fk_fornecedor = id_fornecedor; -- Tem que igualar a relação, chave primária = chave estrangeira
